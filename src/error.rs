701e3bb565ef010e77687c4bf4f07b30fac73d89
roblabla <unfiltered@roblab.la>
2018-12-10 23:51:39 +0000

    PKI: Stop using openssl, use pure-rust RustCrypto alternative.
    OpenSSL is a pain in the ass to build on Travis for windows. Not to mention,
    it's, like, C. Eww. RustCrypto crate `aes` seems to do the job.

--- src/error.rs
+++ src/error.rs
@@ -1,24 +1,18 @@
 use std::io;
 use ini;
 use failure::Backtrace;
-use openssl::error::ErrorStack;
+use block_modes::BlockModeError;
 
 #[derive(Debug, Fail)]
 pub enum Error {
     #[fail(display = "Error reading keys file: {}", _0)]
     Io(io::Error, Backtrace),
-    #[fail(display = "Key derivation error: {}", _0)]
-    Openssl(ErrorStack, Backtrace),
+    #[fail(display = "Decryption failed")]
+    BlockMode(BlockModeError, Backtrace),
     #[fail(display = "Error parsing the INI file: {}", _0)]
     Ini(ini::ini::Error, Backtrace),
     #[fail(display = "Key derivation error: {}", _0)]
     Crypto(String, Backtrace),
-}
-
-impl From<ErrorStack> for Error {
-    fn from(err: ErrorStack) -> Error {
-        Error::Openssl(err, Backtrace::new())
-    }
 }
 
 impl From<io::Error> for Error {
@@ -30,5 +24,11 @@ impl From<io::Error> for Error {
 impl From<ini::ini::Error> for Error {
     fn from(err: ini::ini::Error) -> Error {
         Error::Ini(err, Backtrace::new())
+    }
+}
+
+impl From<BlockModeError> for Error {
+    fn from(err: BlockModeError) -> Error {
+        Error::BlockMode(err, Backtrace::new())
     }
 }
