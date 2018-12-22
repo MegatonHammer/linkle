dfdd6d67924b47a38d2dbebe2096e05ec011193c
roblabla <unfiltered@roblab.la>
2018-12-22 18:39:27 +0000

    CLAP: Add a simple extract command.

--- src/bin/linkle_clap.rs
+++ src/bin/linkle_clap.rs
@@ -2,7 +2,7 @@ extern crate structopt;
 
 extern crate linkle;
 
-use std::fs::{OpenOptions};
+use std::fs::{OpenOptions, File};
 use std::path::{Path, PathBuf};
 use std::process;
 use structopt::StructOpt;
@@ -66,6 +66,37 @@ enum Opt {
         #[structopt(parse(from_os_str))]
         output_file: PathBuf,
     },
+    /// Extract an NCA file.
+    #[structopt(name = "nca_extract")]
+    NcaExtract {
+        /// Sets the input file to use.
+        #[structopt(parse(from_os_str))]
+        input_file: PathBuf,
+
+        /// Sets the output file to extract the header to.
+        #[structopt(parse(from_os_str), long = "header-json")]
+        header_file: Option<PathBuf>,
+
+        /// Sets the output file to extract the section0 to.
+        #[structopt(parse(from_os_str), long = "section0")]
+        section0_file: Option<PathBuf>,
+
+        /// Sets the output file to extract the section1 to.
+        #[structopt(parse(from_os_str), long = "section1")]
+        section1_file: Option<PathBuf>,
+
+        /// Sets the output file to extract the section2 to.
+        #[structopt(parse(from_os_str), long = "section2")]
+        section2_file: Option<PathBuf>,
+
+        /// Sets the output file to extract the section3 to.
+        #[structopt(parse(from_os_str), long = "section3")]
+        section3_file: Option<PathBuf>,
+
+        /// Use development keys instead of retail
+        #[structopt(short = "d", long = "dev")]
+        dev: bool
+    }
 }
 
 fn create_nxo(format: &str, input_file: &str, output_file: &str, icon_file: Option<&str>, romfs_dir: Option<&str>, nacp_file: Option<&str>) -> Result<(), linkle::error::Error> {
@@ -124,17 +155,56 @@ fn create_romfs(input_directory: &Path, output_file: &Path) -> Result<(), linkle
     Ok(())
 }
 
-fn to_opt_str(s: &Option<String>) -> Option<&str> {
-    s.as_ref().map(String::as_ref)
+fn to_opt_ref<U: ?Sized, T: AsRef<U>>(s: &Option<T>) -> Option<&U> {
+    s.as_ref().map(AsRef::as_ref)
+}
+
+fn extract_nca(input_file: &Path, is_dev: bool, output_header_json: Option<&Path>,
+               output_section0: Option<&Path>, output_section1: Option<&Path>,
+               output_section2: Option<&Path>, output_section3: Option<&Path>) -> std::io::Result<()> {
+    let keys = if is_dev {
+        linkle::pki::Keys::new_dev(None).unwrap()
+    } else {
+        linkle::pki::Keys::new_retail(None).unwrap()
+    };
+    let nca = linkle::format::nca::Nca::from_file(&keys, File::open(input_file)?).unwrap();
+    if let Some(output_header_json) = output_header_json {
+        let mut output_header_json = File::create(output_header_json)?;
+        nca.write_json(&mut output_header_json).unwrap();
+    }
+    if let Some(output_section0) = output_section0 {
+        let mut output_section0 = File::create(output_section0)?;
+        let mut section = nca.section(0).unwrap();
+        std::io::copy(&mut section, &mut output_section0)?;
+    }
+    if let Some(output_section1) = output_section1 {
+        let mut output_section1 = File::create(output_section1)?;
+        let mut section = nca.section(1).unwrap();
+        std::io::copy(&mut section, &mut output_section1)?;
+    }
+    if let Some(output_section2) = output_section2 {
+        let mut output_section2 = File::create(output_section2)?;
+        let mut section = nca.section(2).unwrap();
+        std::io::copy(&mut section, &mut output_section2)?;
+    }
+    if let Some(output_section3) = output_section3 {
+        let mut output_section3 = File::create(output_section3)?;
+        let mut section = nca.section(3).unwrap();
+        std::io::copy(&mut section, &mut output_section3)?;
+    }
+    Ok(())
 }
 
 fn process_args(app: &Opt) {
     let res = match app {
-        Opt::Nro { ref input_file, ref output_file, ref icon, ref romfs, ref nacp } => create_nxo("nro", input_file, output_file, to_opt_str(icon), to_opt_str(romfs), to_opt_str(nacp)),
+        Opt::Nro { ref input_file, ref output_file, ref icon, ref romfs, ref nacp } => create_nxo("nro", input_file, output_file, to_opt_ref(icon), to_opt_ref(romfs), to_opt_ref(nacp)),
         Opt::Nso { ref input_file, ref output_file } => create_nxo("nro", input_file, output_file, None, None, None),
         Opt::Pfs0 { ref input_directory, ref output_file } => create_pfs0(input_directory, output_file),
         Opt::Nacp { ref input_file, ref output_file } => create_nacp(input_file, output_file),
         Opt::Romfs { ref input_directory, ref output_file } => create_romfs(input_directory, output_file),
+        Opt::NcaExtract { ref input_file, ref header_file, ref section0_file,
+                          ref section1_file, ref section2_file,
+                          ref section3_file, dev } => extract_nca(input_file, *dev, to_opt_ref(header_file), to_opt_ref(section0_file), to_opt_ref(section1_file), to_opt_ref(section2_file), to_opt_ref(section3_file)),
     };
 
     if let Err(e) = res {
